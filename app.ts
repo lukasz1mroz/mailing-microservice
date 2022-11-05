662700ad7e24207571d2a470cca74b7d05fb2da4
Lukasz Mroz <lukasz.mroz@merck.com>
2022-11-05 11:17:15 +0100

    Add body parser, nodemailer

--- app.ts
+++ app.ts
@@ -1,13 +1,16 @@
 import express from 'express';
 import expressWinston from 'express-winston';
+import bodyParser from 'body-parser';
 
-import Router from './src/controller/Router';
+import Router from './src/controller/router';
 import { expressErrorHandler } from './src/utils/errorHandler';
 import { expressWinstonConfig } from './src/utils/logger';
 
 export const expressApp = () => {
   const app = express();
   const port = 3000;
+
+  app.use(bodyParser.json());
 
   app.use('/', Router);
 
